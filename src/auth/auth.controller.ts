fe1b2e8cc0f0096797b4fa12160e3ee60926c33d
Raimundo Neto <rasneto147@gmail.com>
2022-10-18 17:47:22 -0300

    local strategy and local guards added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Controller, Get, Post, Body, HttpCode} from '@nestjs/common';
+import { Controller, Get, Post, Body, HttpCode, UseGuards, Request} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @UseGuards(LocalAuthGuard)
   @Post('login')
   @HttpCode(200)
-  login(@Body() loginDto: any) {
-    return this.authService.validateUser(loginDto.email, loginDto.password);
+  login(@Request() req) {
+    return req.user;
   }
 }
